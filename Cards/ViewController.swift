81fc93b36acae3d3e6e689a46db32ed7b09fccfe
leandro.estrada <leandro.a.estrada@gmail.com>
2020-10-23 17:58:31 -0300

    Adding initial files

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -11,55 +11,14 @@ import Foundation
 
 class ViewController: UIViewController {
     
-    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
-    
-    
-    
-    var flipCount = 0{
-        didSet{
-            flipCountLabel.text = "Flips: \(flipCount)"
-}
-    }
-    @IBOutlet var cardButtons: [UIButton]!
-    
-   // var emojiChoices: Array<String> = ["👻","🍔","👻","🍔"]
-    
-    @IBAction func touchCard(_ sender: UIButton){
-        flipCount += 1
-        if let cardNumber = cardButtons.index(of: sender){
-            game.chooseCard(at: cardNumber)
-            updateViewFromModel()
-        }else{
-            print("Chosen card was not in cardButtons")
-        }
+    @IBAction func touchCard(_ sender: UIButton) {
+        print( )
     }
     
-    func updateViewFromModel(){
-        for index in cardButtons.indices{
-            let button = cardButtons[index]
-            let card = game.cards[index]
-            if card.isFaceUp{
-                button.setTitle(emoji(for: card), for: UIControlState.normal)
-                button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
-            }
-            else{
-                button.setTitle("", for: UIControlState.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) :  #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
-            }
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
     }
-    
-    var emojiChoices = ["🧟‍♂️","🧙🏾‍♂️", "👹", "🦇"]
-    
-    func emoji(for card: Card)-> String{
-            return "?"
-
-    }
-    
-    @IBOutlet weak var flipCountLabel: UILabel!
-    
-    
-    
     
 }
 
